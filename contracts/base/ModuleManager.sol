b390b0527eacd03d8b89bd661b3ae0b82555a2cd
leekt <leekt216@gmail.com>
2023-02-06 11:14:43 +0900

    fix: setupModule does not require gasleft()

--- contracts/base/ModuleManager.sol
+++ contracts/base/ModuleManager.sol
@@ -22,7 +22,7 @@ contract ModuleManager is SelfAuthorized, Executor {
         modules[SENTINEL_MODULES] = SENTINEL_MODULES;
         if (to != address(0))
             // Setup has to complete successfully or transaction fails.
-            require(execute(to, 0, data, Enum.Operation.DelegateCall, gasleft()), "GS000");
+            require(execute(to, 0, data, Enum.Operation.DelegateCall, type(uint256).max), "GS000");
     }
 
     /// @dev Allows to add a module to the whitelist.
